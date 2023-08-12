1626f7dcb96d02e2d0086313fb1854612072825a
Damenjo Sitepu <damenstory@gmail.com>
2023-08-12 17:48:39 +0700

    Feat: add express package and create first request

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello Express!");
+import express from "express";
+import { CONFIG } from "./config";
+
+const finalApp = express();
+
+// First request on the root 
+finalApp.get("/", (req, res) => {
+    return res.send("Root route!");
+});
+
+finalApp.listen(CONFIG.PORT, () => {
+    console.log("Final app has been released!");
+});
