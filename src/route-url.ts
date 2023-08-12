7445fbae100791599029e2b1232ee14da74f7753
Damenjo Sitepu <damenstory@gmail.com>
2023-08-12 22:35:26 +0700

    Feat: add route url

--- src/route-url.ts
+++ src/route-url.ts
@@ -4,5 +4,6 @@ export const ROUTE_URL = {
     FAKE_ALL_API: "/api/department",
     REAL_CHART_OF_ACCOUNTS: "/api/chart-of-accounts",
     REAL_REDIRECT_TO_GOOGLE: "/api/redirect/google",
-    REAL_CHART_OF_ACCOUNT: "/api/chart-of-account"
+    REAL_CHART_OF_ACCOUNT: "/api/chart-of-account",
+    REAL_CHART_OF_ACCOUNT_WITH_ID: "/api/chart-of-account/:id"
 };
