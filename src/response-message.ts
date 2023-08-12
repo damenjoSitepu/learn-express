1c75041af88613ba3c944723ddd26f9dfa805734
Damenjo Sitepu <damenstory@gmail.com>
2023-08-12 22:35:36 +0700

    Feat: add response message

--- src/response-message.ts
+++ src/response-message.ts
@@ -2,5 +2,8 @@ export const RESPONSE_MESSAGE = {
     SUCCESS_GET_CHART_OF_ACCOUNTS: "Successfully get chart of accounts data!",
     SUCCESS_CREATE_CHART_OF_ACCOUNT: "Successfully create chart of account!",
     SUCCESS_UPDATE_CHART_OF_ACCOUNT: "Successfully update chart of account!",
-    SUCCESS_DELETE_CHART_OF_ACCOUNT: "Successfully delete chart of account!"
+    SUCCESS_DELETE_CHART_OF_ACCOUNT: "Successfully delete chart of account!",
+    SUCCESS_GET_CHART_OF_ACCOUNT: "Successfully get chart of account",
+    CHART_OF_ACCOUNT_ARE_EMPTY: "Chart of accounts are empty!",
+    CHART_OF_ACCOUNT_NOT_FOUND: "Chart of account not found!"
 };
