b17dd47d96af62e2bcb8a34e88cd7539ead3bc7b
Damenjo Sitepu <damenstory@gmail.com>
2023-08-12 22:27:02 +0700

    Feat: add fake chart of accounts data

---
+++ src/data/chart-of-accounts/chart-of-accounts.interface.ts
@@ -0,0 +1,5 @@
+export interface ChartOfAccounts {
+    id: number;
+    name: string;
+    isActive: boolean;
+};
---
+++ src/data/chart-of-accounts/chart-of-accounts.ts
@@ -0,0 +1,19 @@
+import { ChartOfAccounts } from "./chart-of-accounts.interface";
+
+export const CHART_OF_ACCOUNTS: ChartOfAccounts[] = [
+    {
+        id: 1,
+        name: "Wi-Fi Expense",
+        isActive: true
+    },
+    {
+        id: 2,
+        name: "Group A Sales",
+        isActive: true
+    },
+    {
+        id: 3,
+        name: "Group A Revenue",
+        isActive: false
+    }
+];
